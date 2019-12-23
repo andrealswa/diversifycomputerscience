66a0464630b77bb213e8cbebac72086b87c7ac07
Dan <ddupont2@uwo.ca>
2019-12-22 21:07:46 -0500

    added linear stepper

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -19,7 +19,8 @@ import {
   MatSortModule,
   MatPaginatorModule,
   MatSnackBarModule,
-  MatGridListModule
+  MatGridListModule,
+  MatStepperModule
 } from "@angular/material";
 
 @NgModule({
@@ -43,7 +44,8 @@ import {
     MatSortModule,
     MatPaginatorModule,
     MatSnackBarModule,
-    MatGridListModule
+    MatGridListModule,
+    MatStepperModule
   ],
   exports: [
     MatButtonModule,
@@ -65,7 +67,8 @@ import {
     MatSortModule,
     MatPaginatorModule,
     MatSnackBarModule,
-    MatGridListModule
+    MatGridListModule,
+    MatStepperModule
   ]
 })
 export class MaterialModule {}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-welcome",
@@ -7,10 +8,24 @@ import { Component, OnInit } from "@angular/core";
 })
 export class WelcomeComponent implements OnInit {
   breakpoint: number;
-  constructor() {}
+  isLinear = false; // Linear stepper
+  firstFormGroup: FormGroup; // Linear stepper
+  secondFormGroup: FormGroup; // Linear stepper
+
+  // The private _formBuilder: Formbuilder is for the linear stepper.
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.breakpoint = window.innerWidth <= 400 ? 1 : 2;
+
+    // Linear stepper code start
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ["", Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ["", Validators.required]
+    });
+    // Linear stepper code end
   }
 
   onResize(event) {
