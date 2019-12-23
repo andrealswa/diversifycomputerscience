60cfd3a8f7757f17d7ec2f881428ca630f10f308
Dan <ddupont2@uwo.ca>
2019-12-22 20:23:53 -0500

    front end design

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
+  styleUrls: ["./welcome.component.css"]
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
+  breakpoint: number;
+  constructor() {}
 
   ngOnInit() {
+    this.breakpoint = window.innerWidth <= 400 ? 1 : 2;
   }
 
+  onResize(event) {
+    this.breakpoint = event.target.innerWidth <= 400 ? 1 : 2;
+  }
 }
