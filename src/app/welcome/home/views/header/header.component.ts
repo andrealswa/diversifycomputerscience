d9a3c297925a5fa63d7982556d6541fd673971c4
Dan <ddupont2@uwo.ca>
2019-12-31 16:39:28 -0500

    saving before attempting something with a 98% chance of causing the code to explode

--- src/app/welcome/home/views/header/header.component.ts
+++ src/app/welcome/home/views/header/header.component.ts
@@ -12,36 +12,51 @@ import {
   WINDOW
 } from "../../../shared/helpers/window.helper";
 
+// From CLI generated navbar
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"]
 })
-export class HeaderComponent implements OnInit {
-  isFixed;
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    @Inject(WINDOW) private window: Window
-  ) {}
+export class HeaderComponent {
+  // isFixed;
+  // constructor(
+  //   @Inject(DOCUMENT) private document: Document,
+  //   @Inject(WINDOW) private window: Window
+  // ) {}
 
-  ngOnInit() {}
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-    const offset =
-      this.window.pageYOffset ||
-      this.document.documentElement.scrollTop ||
-      this.document.body.scrollTop ||
-      0;
-    if (offset > 10) {
-      this.isFixed = true;
-    } else {
-      this.isFixed = false;
-    }
-  }
+  // ngOnInit() {}
+  // @HostListener("window:scroll", [])
+  // onWindowScroll() {
+  //   const offset =
+  //     this.window.pageYOffset ||
+  //     this.document.documentElement.scrollTop ||
+  //     this.document.body.scrollTop ||
+  //     0;
+  //   if (offset > 10) {
+  //     this.isFixed = true;
+  //   } else {
+  //     this.isFixed = false;
+  //   }
+  // }
 
-  @HostBinding("class.menu-opened") menuOpened = false;
+  // @HostBinding("class.menu-opened") menuOpened = false;
 
-  toggleMenu() {
-    this.menuOpened = !this.menuOpened;
-  }
+  // toggleMenu() {
+  //   this.menuOpened = !this.menuOpened;
+  // }
+
+  // Angular Generated Navbar
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
