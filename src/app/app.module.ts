7a47c5f7f9e909ef655841438e31f7e53f6e974f
Dan <ddupont2@uwo.ca>
2020-01-02 03:03:25 -0500

    removed unused navigation folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,6 @@ import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material.module";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { HeaderComponent } from "./navigation/header/header.component";
-import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
 import { environment } from "../environments/environment";
 import { UIService } from "./shared/ui.service";
 import { UserEntryFormComponent } from "./user-entry-form/user-entry-form.component";
@@ -28,8 +26,6 @@ import { AuthenticationModule } from "./authentication/authentication.module";
   declarations: [
     AppComponent,
     WelcomeComponent,
-    HeaderComponent,
-    SidenavListComponent,
     UserEntryFormComponent,
     TableComponent,
     NotFoundComponent,
