77c12ec204c0d0938226c11f607aa6c37ec85a53
Dan <ddupont2@uwo.ca>
2020-01-02 02:34:31 -0500

    removed comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     component: ContactComponent
   },
   {
-    path: "**", // double stars for not found pages
+    path: "**",
     component: NotFoundComponent
   }
 ];
