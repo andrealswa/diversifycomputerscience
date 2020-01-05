f6e1046bc296f872ef6079f2bc01ad96e0f43e06
andrealswa <swartz8@uwindsor.ca>
2020-01-05 18:05:18 -0500

    included google places for entry form

--- src/app/user-entry-form/user-entry-form.component.ts
+++ src/app/user-entry-form/user-entry-form.component.ts
@@ -192,7 +192,7 @@ export class UserEntryFormComponent implements OnInit {
   selfIDList: string[] = [
     "Asian",
     "Indigenous / Native",
-    "Lesbian, Gay, Bisexual, Transgender, Queer, LGBTQ+",
+    "Lesbian, Gay, Bisexual, Transgender, Queer (LGBTQ+)",
     "Multi-Racial",
     "Other Race",
     "Person with a Disability",
@@ -203,5 +203,20 @@ export class UserEntryFormComponent implements OnInit {
     "Pacific Islander",
     "Other Non-White Self-Identification"
   ];
-  genderList: string[] = ["Male", "Female", "Non-binary", "Self Describe"];
+  genderList: string[] = [
+    "Male",
+    "Female",
+    "Non-binary",
+    "Prefer Not To Disclose"
+  ];
+
+  branchList: string[] = [
+    "Artificial Intelligence",
+    "Cyber Security",
+    "Application Development",
+    "Graphics",
+    "Games",
+    "Systems Programming",
+    "Data Science"
+  ];
 }
