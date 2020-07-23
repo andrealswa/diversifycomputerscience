28a98f54635a04f504ec92cfba829ba9299c63dd
andrealswa <swartz8@uwindsor.ca>
2020-07-22 20:19:48 -0400

    edited disclaimer

--- src/app/welcome/home/views/disclaimer/disclaimer.component.ts
+++ src/app/welcome/home/views/disclaimer/disclaimer.component.ts
@@ -5,38 +5,39 @@ import { Component, OnInit, Input } from "@angular/core";
 
   styleUrls: ["./disclaimer.component.scss"],
   template: `
-    <section class="full-view">
-      <div style="height: 100px"></div>
+    <div class="full-view">
       <mat-card class="container container-outline">
-        <div>
-          <mat-card class="inner-cards-team-bios">
-            <mat-card-header>
-              <mat-card-title>Disclaimer</mat-card-title>
-            </mat-card-header>
-            <mat-card-content>
-              Participants on this list are exclusively self-nominated. All
-              entries are vetted by team members before being made available.
-              Once vetted entries may be edited, updated or removed from the
-              list by their owner.
-            </mat-card-content>
-            <mat-card-content>
-              Though best efforts are made to ensure the accuracy and
-              consistency of information on this list, Diversify CS makes no
-              claims or guarantees about the list as a whole or any specific
-              entry.
-            </mat-card-content>
-            <mat-card-content>
-              Participants consent to having the information on this list
-              publicly displayed and available. Diversify CS cannot guarantee
-              that copies or reproductions will not be made at any given time,
-              and takes no responsibility for the accuracy, representation or
-              removal of these materials.
-            </mat-card-content>
-          </mat-card>
-        </div>
+        <h3 class="disclaimer-title">
+          Disclaimer
+        </h3>
+        <mat-card-content>
+          Participants on this list are exclusively self-nominated. All entries
+          are vetted by team members before being made available. Once vetted
+          entries may be edited, updated or removed from the list by their
+          owner.
+        </mat-card-content>
+        <mat-card-content>
+          Though best efforts are made to ensure the accuracy and consistency of
+          information on this list, Diversify CS makes no claims or guarantees
+          about the list as a whole or any specific entry.
+        </mat-card-content>
+        <mat-card-content>
+          Participants consent to having the information on this list publicly
+          displayed and available. Diversify CS cannot guarantee that copies or
+          reproductions will not be made at any given time, and takes no
+          responsibility for the accuracy, representation or removal of these
+          materials.
+        </mat-card-content>
       </mat-card>
-    </section>
-  `
+      <div class="center-image">
+        <img
+          class="disclaimer-image"
+          src="../../../../../assets/images/undraw_public_discussion_btnw.svg"
+          alt="online collaboration"
+        />
+      </div>
+    </div>
+  `,
 })
 export class DisclaimerComponent implements OnInit {
   constructor() {}
